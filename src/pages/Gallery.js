9dde2ea7a643cbf07c4b05b092297bde87201430
fox-yokai <pat.w.cain@gmail.com>
2020-10-09 21:39:55 -0500

    working gallary prototype

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+import GallaryCard from '../components/gallaryCard/gallaryCard';
 
 class Gallery extends React.Component {
     render() {
         return (
-        <h1>This will be the gallery page!</h1>
+            <div>
+            <Container>
+                <Col md={{span: 7, offset: 3}}>
+                    <br />
+                    <br />
+                    <GallaryCard />
+                </Col>
+            </Container>
+            
+        </div>
         );
     }
 }
