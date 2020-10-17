49680bee73e2b2654201548b83bd93e4ab532b3d
fox-yokai <pat.w.cain@gmail.com>
2020-10-17 13:49:01 -0500

    pricing page place holder created

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+import PricesCard from "../components/pricesCard/pricesCard"
 
 class Pricing extends React.Component {
     render() {
         return(
-            <h1>This will be the pricing page!</h1>
+            <div>
+            <Container>
+                <Col md={{span: 7, offset: 3}}>
+                    <br />
+                    <br />
+                    <PricesCard />
+                </Col>
+            </Container>
+            </div>
         );
     }
 }
