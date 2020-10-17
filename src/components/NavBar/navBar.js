53855913246052385043f3b7cb6fef6658b666f9
fox-yokai <pat.w.cain@gmail.com>
2020-10-17 11:53:01 -0500

    added twitter icon and link to nav bar

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Logo from '../../images/logo-banner.png';
+import { TiSocialTwitter } from "react-icons/ti";
+import { IconContext } from "react-icons";
 
 class NavComponent extends React.Component {
     render() {
@@ -24,6 +26,13 @@ class NavComponent extends React.Component {
                     <Nav.Link href="/tos">Terms of Service</Nav.Link>
                     <Nav.Link href="/contact">Contact</Nav.Link>
                     <Nav.Link href="/about">About</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <IconContext.Provider value={{ color: "blue", size: "1.5em" , className: "react-icons" }}>
+                            <Nav.Link href="https://twitter.com/f0xvalley" target="_blank">
+                                <TiSocialTwitter />
+                            </Nav.Link>
+                        </IconContext.Provider>      
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
