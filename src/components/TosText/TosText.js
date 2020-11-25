3b97a2a25bc7d66ceee6d86c39a741e1c574c473
fox-yokai <pat.w.cain@gmail.com>
2020-11-25 15:26:47 -0600

    created component with the ToS

---
+++ src/components/TosText/TosText.js
@@ -0,0 +1,47 @@
+import React, { Fragment } from 'react';
+
+const TosText = () => {
+        return (        
+            <Fragment>
+            <p>By commissioning me you are automatically agreeing to my terms of service. Please make sure you read through and agree to everything listed below.
+            </p>
+            <br />
+            <p><strong>ALLERGY WARNING:</strong> My roommate and I have two cats, and I am unable to keep them out of my workspace. Please keep this in mind before commissioning me. I am not responsible for any allergic reaction you may have to my products.
+            </p>
+            <br />
+            <h2>Payments</h2>
+            <br />
+            <li><strong>NO REFUNDS</strong> will be given without valid reason.</li>
+            <li>I do not take payment plans unless discussed prior. A 20% deposit will be required upon commissioning me. </li>
+            <li>20% of your commission price is non-refundable once materials have been purchased. A confirmation will be sent once materials have been purchased.</li>
+            <li>Payment plans are a maximum of 4 months.</li>
+            <li>Payment must be made by the agreed date, failure to do so will inquire an additional fee.</li>
+            <li>Payment plans do not count towards wait time.</li>
+            <li>Product is shipped via USPS and is to be paid by the commissioner.</li>
+            <li>In person pickup is free during an event I attend.</li>
+            <li>All payments are strictly through Paypal invoices set up by Fox Valley.</li>
+            <br />
+            <h2>Communication</h2>
+            <br />
+            <li>Commissioners MUST be 18 or older. We will not work with minors (or their parents). If we suspect you to be under 18 we WILL ask for ID, along with a picture of you holding said ID.</li>
+            <li>Communication is strictly through Twitter or E-Mail.</li>
+            <li>If you have any questions or concerns feel free to message me at any point but please give me 24 hours to respond.</li>
+            <li>I will send WIPs and wait for approval before moving onto the next step if needed.</li>
+            <li>All progress is available on my Trello.</li>
+            <br />
+            <h2>Deadlines</h2>
+            <br />
+            <li>I do not agree to deadlines unless discussed prior to commissioning me.</li>
+            <li>If a deadline was discussed prior I will keep my promise to honor that deadline. If any problems arise I will inform you of those delays if they affect your spot in the queue in any way.</li>
+            <li>If your commission is not paid off by the time you agreed to pay me, your project will be pushed back</li>
+            <li>Work takes 1-3 months on average.</li>
+            <br />
+            <h2>Warranty</h2>
+            <br />
+            <li>My suits come with a 1 year warranty and covers any defects that may have been made by me, they will NOT cover any damages caused on purpose, misuse, mistreatment, etc.</li>
+            <li>All repairs are free - you will need to pay for shipping</li>
+            </Fragment>
+        );
+}
+
+export default TosText;
