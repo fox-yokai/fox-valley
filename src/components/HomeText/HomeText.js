914b080f53b8e3d63920000ac9e4c2802e402d0c
fox-yokai <pat.w.cain@gmail.com>
2020-11-30 13:13:35 -0600

    created HomeText component

---
+++ src/components/HomeText/HomeText.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react';
+
+const HomeText = () => {
+    return (
+        <Fragment>
+
+            <div>
+                <h2>Welcome!</h2>
+                <p>I'm Axel, but I go by Fox! I'm a self-taught plush artist. I have been making plush toys since 2019.</p>
+                <p>Though plush making is only a hobby of mine, I love to learn and expand my skills and knowledge</p>
+            </div>
+            <div className="justify-content-center">
+                <h4>Plush Commissions:</h4>
+                <p><strong>Closed</strong></p>
+                <p>Next opening to be announced</p>
+            </div>
+            <div>
+                <h3>Follow me on Instagram and Twitter!</h3>
+            </div>
+
+
+        </Fragment>
+    );
+};
+
+export default HomeText;
