32ffa51caa561269a413bb13683552e8356e5b5e
fox-yokai <pat.w.cain@gmail.com>
2020-11-30 13:18:51 -0600

    added image prop to component

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -7,6 +7,7 @@ const Page = (props) => {
             <div className="row">
                 <div className="col d-flex justify-content-center mt-5 mb-5">
                     <div className="card" style={{"width": "36rem"}}>
+                        <img src={props.logoImg} className="card-img-top" alt={props.logoImgAlt} />
                         <div className="card-body">
                             <h2 className="card-title">{props.heading}</h2>
                                 {props.children}
