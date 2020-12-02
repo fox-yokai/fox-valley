b210836036d2ca3808c7b7ae4a3581d0403fe67f
fox-yokai <pat.w.cain@gmail.com>
2020-12-02 13:35:45 -0600

    refactored to use the PricesText component

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
 import PricesCard from "../components/pricesCard/pricesCard"
 
+import Page from '../components/Page/Page';
+import PricesText from '../components/PricesText/PricesText';
+
 class Pricing extends React.Component {
     render() {
         return(
             <div>
-            <Container>
+                <Page heading="Prices">
+                    <PricesText />
+                </Page>
+            {/* <Container>
                 <Col md={{span: 7, offset: 3}}>
                     <br />
                     <br />
                     <PricesCard />
                 </Col>
-            </Container>
+            </Container> */}
             </div>
         );
     }
