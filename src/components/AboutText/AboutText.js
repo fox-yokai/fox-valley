fd5e97a6400540aa0f77eecd94795acaed761ca5
fox-yokai <pat.w.cain@gmail.com>
2020-12-04 11:20:38 -0600

    created new component

---
+++ src/components/AboutText/AboutText.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+const AboutText = () => {
+    return (
+        <Fragment>
+            <div className='col'>
+                <div className='row'>
+                <p>I'm Axel, but I go by Fox! I'm a self-taught plush artist. I have been making plush toys since 2019.</p>
+                <p>Though plush making is only a hobby of mine, I love to learn and expand my skills and knowledge</p>
+                </div>
+            </div>
+
+        </Fragment>
+    );
+};
+
+export default AboutText;
