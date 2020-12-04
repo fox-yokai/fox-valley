be33bb404637bbadf8ee571a92a493d8bbfc8b34
fox-yokai <pat.w.cain@gmail.com>
2020-12-04 11:07:37 -0600

    created ContactText component

---
+++ src/components/ContactText/ContactText.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import TwitterIcon from '../TwitterIcon/TwitterIcon';
+import InstagramIcon from '../InstagramIcon/InstagramIcon';
+
+
+const ContactText = () => {
+    return (
+        <Fragment>
+            <div className='col'>
+                <div className='row'>
+                    <p>Interested in commissioning a custom handmade plush from us or have any questions? </p>
+                    <p>Please email: <a href="mailto:axel@foxvalley.studio?subject=Inquiry%20From%20Website">axel@foxvalley.studio</a></p> 
+                </div>
+            </div>
+
+        </Fragment>
+    );
+};
+
+export default ContactText;
